fe859036010689f1e4f766a1e13c8bd8eed47b4b
Jim Klein <j.mesklein@gmail.com>
2017-01-26 15:54:26 -0800

    updated panels to dynamically show scroll bar

--- src/app/emr/panels/problems.component.ts
+++ src/app/emr/panels/problems.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { Problem } from '../../models/problem';
 
 @Component({
@@ -7,16 +7,32 @@ import { Problem } from '../../models/problem';
     styleUrls: ['../emr.component.css'],
 })
 
-export class ProblemsComponent {
+export class ProblemsComponent implements OnInit {
     @Input() title: string;
-    @Input() headers: string [];
+    @Input() headers: string[];
     @Input() problems: any[];
+    @ViewChild('panel') private panelContainer: ElementRef;
+    private toggleScroll: string = "false";
 
     generateArray(obj) {
-        return Object.keys(obj).map((key) => { 
-            if(key != "headers"){
-                return obj[key] ;
+        return Object.keys(obj).map((key) => {
+            if (key != "headers") {
+                return obj[key];
             }
         });
+    }
+
+    // toggleScroll(): boolean {
+    //     let element = this.panelContainer.nativeElement;
+
+    //     return element.offsetWidth > 200 ? true : false;
+    // }
+
+    ngOnInit() {
+                let e = this.panelContainer.nativeElement;
+        this.toggleScroll = e.offsetWidth < 630  ? "scroll" : "hidden";
+    }
+
+    ngOnChanges() {
     }
 }
